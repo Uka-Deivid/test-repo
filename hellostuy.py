069f9102cce1928262ca1098872aaa48ccfa1e18
Deivid Uka <duka80@stuy.edu>
2026-09-15 23:54:05 -0400

    Remotely added another print statement; am now comfortable using terminal for all github applications

--- hellostuy.py
+++ hellostuy.py
@@ -1,1 +1,3 @@
-print("Hello Stuyvesant!")
+print("Hello Stuyvesant!") 
+print("""Using ssh I can edit from whichever computer I want without login! This is another
+commit made at home connected to the lab by ssh.""")
